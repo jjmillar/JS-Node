519f89b8f046c2fd031bffc3f0c248604c52c59e
Julian Millar <julianjmillar@gmail.com>
2022-09-09 19:11:08 -0300

    Tipos de datos y datos estructurales

---
+++ 01_TiposDeDatos.js
@@ -0,0 +1,40 @@
+/**
+ * Tipos de datos
+ */
+
+// numero - number
+console.log(27);
+console.log(27.56);
+
+// texto - string
+console.log("Julian Millar");
+console.log("Julian Millar");
+console.log(`Hola, soy Julian`);
+
+// logicos - booleans
+console.log(true);
+console.log(false);
+
+// indefinido - undefined
+console.log(undefined);
+
+// null
+console.log(null);
+
+/**
+ * Tipos de datos estructurales
+ */
+
+// Objeto - object
+console.log({
+  nombre: "Julian",
+  edad: 27,
+  fechaDeNacimiento: null,
+  esAlto: false,
+  ciudades: ["Quito", "Guayaquil", "Bueno Aires"],
+  direccion: { callePrincipal: "Balmaceda" },
+});
+
+// Arreglo - dato de tipo lista - array
+console.log(["Mexico", "Chile", "Venezuela"]);
+console.log([21, true, { nombre: "Gabriel" }, [1, 2, 3, 4]]);
